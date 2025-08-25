ed49bd3c9fc00df5e305df3b8b0b81ecac02522d
SpongezCr <gavinn0000@gmail.com>
2025-08-25 16:00:26 +0800

    Change port to process.env.PORT

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ const {indexRouter, newMessageRouter} = require('./routes/routers');
 app.use('/new', newMessageRouter);
 app.use('/', indexRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log("Now running on port " + PORT));
